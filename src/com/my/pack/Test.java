712ca2f3383ea0415edeff01ced8e119432fa347
SRS <SRS@S510PWB13014528>
2017-08-31 16:10:44 +0530

    update

---
+++ src/com/my/pack/Test.java
@@ -0,0 +1,16 @@
+package com.my.pack;
+
+public class Test {
+      
+	public static void main(String[] args)
+	{
+		
+	int[] arr= new int[]{1,2,3,4,5,6};
+	
+    System.out.println(arr[3]);
+    
+    int arrayLength = arr.length;
+    System.out.println(arrayLength);
+	}
+
+}
